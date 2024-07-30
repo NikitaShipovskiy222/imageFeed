40e9e3aeca567c493e64ed125c36e4b6eb09bac7
NikitaShipovskiy222 <nikita04.shipovskiy@gmail.com>
2024-07-30 22:20:34 +0300

    Initial Commit

---
+++ imageFeed/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  imageFeed
+//
+//  Created by Nikita Shipovskiy on 30/07/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
